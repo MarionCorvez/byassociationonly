16ab38805a78248fa564be777e5f7cdd314e1056
Marion Corvez <marion.corvez@gmail.com>
2025-07-08 09:01:44 +0200

    Step 0: Define the scope: objectives, features and functionalities

---
+++ app/about-us/page.tsx
@@ -0,0 +1,13 @@
+// import SignIn from "@components/SignIn";
+
+export default function About() {
+  return (
+    <>
+      <main>
+        <section>
+          <h2>About us</h2>
+        </section>
+      </main>
+    </>
+  );
+}
---
+++ app/login/page.tsx
@@ -0,0 +1,13 @@
+// import SignIn from "@components/SignIn";
+
+export default function Login() {
+  return (
+    <>
+      <main>
+        <section>
+          <h2>Sign In</h2>
+        </section>
+      </main>
+    </>
+  );
+}
