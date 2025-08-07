5005cc7f5e379bb5965f0ac479b35ba3262ca780
Marion Corvez <marion.corvez@gmail.com>
2025-08-07 09:52:09 +0200

    Step 6.6: Case Studies Components

--- src/app/(pages)/case-studies/page.tsx
+++ src/app/(pages)/case-studies/page.tsx
@@ -1,8 +1,10 @@
+import StaticCover from "@/components/StaticCover";
 import Cases from "@/components/Cases";
 
 export default function CaseStudies() {
   return (
     <>
+      <StaticCover />
       <Cases />
     </>
   );
